394d84f353b0b95a377d92b92eb0c95ac7678972
Mike Hudson <mikehudsonuk@icloud.com>
2025-11-07 14:06:15 +0000

    Test push

--- src/mono/mono/metadata/object.c
+++ src/mono/mono/metadata/object.c
@@ -57,6 +57,7 @@
 #include "monitor.h"
 #include "icall-decl.h"
 #include "icall-signatures.h"
+//test comment
 #if _MSC_VER
 #pragma warning(disable:4312) // FIXME pointer cast to different size
 #endif
